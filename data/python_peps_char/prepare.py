7f1a5baa073bd338827e1b71f99e6d5ffd2f13bd
Karel Minarik <karel.minarik@gmail.com>
2023-09-03 16:28:22 +0200

    WIP > Python PEPs, download ZIP file

--- data/python_peps_char/prepare.py
+++ data/python_peps_char/prepare.py
@@ -4,7 +4,8 @@ list of Python PEP documents (https://peps.python.org/pep-0001/).
 """
 import os
 import re
-from multiprocessing.pool import ThreadPool
+import tempfile
+import zipfile
 
 import pickle
 import requests
@@ -12,70 +13,105 @@ from tqdm import tqdm
 import numpy as np
 
 
-def fetch_pep_list_from_github():
-    url = "https://api.github.com/repos/python/peps/contents"
-    response = requests.get(url)
-    if response.status_code != 200:
-        raise Exception(f"Failed to fetch data from GitHub API: {response.status_code}")
+def download_zip(url, dest):
+    with requests.get(url, stream=True) as response:
+        response.raise_for_status()
 
-    files = response.json()
-    pep_files = [
-        f["name"]
-        for f in files
-        if f["name"].startswith("pep-")
-        and (f["name"].endswith(".txt") or f["name"].endswith(".rst"))
+        total_size = int(response.headers.get("content-length", 0))
+
+        tqdm_bar = tqdm(total=total_size, desc="Download", unit="iB", leave=False)
+
+        with open(dest, "wb") as f:
+            for chunk in response.iter_content(chunk_size=500 * 1024):  # 500 KB chunks
+                tqdm_bar.update(len(chunk))
+                f.write(chunk)
+
+        tqdm_bar.close()
+
+
+def extract_zip(zip_file_path):
+    temp_dir = tempfile.mkdtemp()
+
+    with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
+        zip_ref.extractall(temp_dir)
+
+    return temp_dir
+
+
+def clean_content(content):
+    patterns = [
+        # Remove metadata from preamble
+        (
+            re.compile(
+                "^(Author:|Status:|Type:|Content-Type:|Created:|Post-History:|Version:|Last-Modified:)(.*?)(?=(^[\w-]+:|^\n))",
+                flags=re.MULTILINE | re.DOTALL,
+            ),
+            "",
+        ),
+        # Remove image blocks
+        (
+            re.compile(
+                r".. image::.*?(\n(?=^[^\s])|\Z)", flags=re.MULTILINE | re.DOTALL
+            ),
+            "",
+        ),
+        # Remove all '::' lines
+        (
+            re.compile(r"^\s*::\s*$", flags=re.MULTILINE),
+            "",
+        ),
+        # Remove metadata from footer
+        (
+            re.compile("^\.\.\s+Local Variables:.*End:", re.MULTILINE | re.DOTALL),
+            "",
+        ),
+        # Remove copyright notices
+        (
+            re.compile(
+                "Copyright\n=+\n*$\s*This document.+\.$", flags=re.MULTILINE | re.DOTALL
+            ),
+            "",
+        ),
+        # Remove consecutive newlines
+        (re.compile("^\n{2,}", flags=re.MULTILINE), "\n"),
+        (re.compile("\n{2,}$", flags=re.MULTILINE), "\n"),
     ]
 
-    return pep_files
-
-
-def download_pep(pep_filename):
-    # print(f"Downloading [{pep_filename}]")
-    base_url = "https://raw.githubusercontent.com/python/peps/main/"
-    url = f"{base_url}{pep_filename}"
-    response = requests.get(url)
-
-    if response.status_code != 200:
-        raise Exception(f"Failed to download {pep_filename}: {response.status_code}")
-
-    return response.text
-
-
-def download_peps_in_parallel(pep_list, num_threads=16):
-    with ThreadPool(num_threads) as pool:
-        pep_contents = list(
-            tqdm(
-                pool.imap(download_pep, pep_list), total=len(pep_list), desc="Download"
-            )
-        )
-    return dict(zip(pep_list, pep_contents))
+    cleaned_string = content
+    for pat, repl in patterns:
+        cleaned_string = pat.sub(repl, cleaned_string)
+    return cleaned_string.strip()
 
 
 input_file_path = os.path.join(os.path.dirname(__file__), "input.txt")
 
 if not os.path.exists(input_file_path):
-    pep_list = fetch_pep_list_from_github()
+    zip_url = "https://codeload.github.com/python/peps/zip/refs/heads/main"
+    zip_dest = tempfile.NamedTemporaryFile(delete=False).name
+    download_zip(zip_url, zip_dest)
+    print(f"Downloaded ZIP to {zip_dest}")
 
-    ignore_peps = [
-        "pep-8001.rst",
-        "pep-8001.rst",
-        "pep-8103.rst",
-        "pep-8104.rst",
-    ]
+    repo_path = extract_zip(zip_dest)
+    print(f"Extracted content to {repo_path}")
 
-    print(f"downloading {len(pep_list)-len(ignore_peps)} PEP documents...")
-    pep_contents = download_peps_in_parallel(
-        [item for item in pep_list if not item in ignore_peps]
+    contents_path = os.path.join(repo_path, "peps-main")
+    pep_files = sorted(
+        [
+            f
+            for f in os.listdir(contents_path)
+            if f.startswith("pep-")
+            and (f.endswith(".txt") or f.endswith(".rst"))
+            and not f.startswith("pep-8")
+        ]
     )
 
-    ignore_lines = [
-        re.compile("^::"),
-        re.compile("^.. "),
-        re.compile("Copyright\n=+^", re.MULTILINE),
-    ]
-
-    with open(input_file_path, "w") as f:
-        f.write("\n\n".join(pep_contents.values()))
+    with tqdm(total=len(pep_files), desc="Process", leave=False) as pbar:
+        for pep_file in pep_files:
+            with open(os.path.join(contents_path, pep_file), "r") as in_file:
+                content = clean_content(in_file.read())
+                with open(input_file_path, "a") as out_file:
+                    out_file.write(content)
+            pbar.update(1)
 
 # -------------------------------------------------------
 
